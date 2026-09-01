9b12babccd16905315fdb26d3ac665c6a359c30a
khanalsushil226-del <khanalsushil226@gmail.com>
2026-09-01 19:48:49 +0545

    making clean

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// ==========================================
-// PROJECT FILTERS
-// ==========================================
 
 const filterButtons = document.querySelectorAll(".filter-btn");
 const projectCards = document.querySelectorAll(".project-card");
@@ -9,18 +6,17 @@ filterButtons.forEach(button => {
 
     button.addEventListener("click", () => {
 
-        // Remove active state from all buttons
+       
         filterButtons.forEach(btn => {
             btn.classList.remove("active");
         });
 
-        // Add active state to clicked button
+       
         button.classList.add("active");
 
         // Get selected category
         const selectedCategory = button.dataset.filter;
 
-        // Show / hide projects
         projectCards.forEach(card => {
 
             const projectCategory = card.dataset.category;
